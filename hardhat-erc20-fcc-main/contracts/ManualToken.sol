965f0bfba3989f74df5f9dae19efbef7e080fe35
paymon <paymon@safexapp.com>
2026-06-29 20:30:24 +0800

    8

--- hardhat-erc20-fcc-main/contracts/ManualToken.sol
+++ hardhat-erc20-fcc-main/contracts/ManualToken.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+// 接口定义：当合约被 approveAndCall 调用时，接收者合约需要实现这个接口
 interface tokenRecipient {
   function receiveApproval(
     address _from,
@@ -11,78 +12,91 @@ interface tokenRecipient {
 }
 
 contract TokenERC20 {
-  // Public variables of the token
+  // 代币名称，例如 "MyToken"
   string public name;
+  // 代币符号，例如 "MTK"
   string public symbol;
+  // 小数位数，ERC20 规范中一般为 18
   uint8 public decimals = 18;
-  // 18 decimals is the strongly suggested default, avoid changing it
+  // 代币总供应量（按最小单位计数）
   uint256 public totalSupply;
 
-  // This creates an array with all balances
+  // 记录每个地址的代币余额
   mapping(address => uint256) public balanceOf;
+  // 记录每个地址授权给其他地址的可花费额度
   mapping(address => mapping(address => uint256)) public allowance;
 
-  // This generates a public event on the blockchain that will notify clients
+  // 转账事件，链上客户端可监听此事件
   event Transfer(address indexed from, address indexed to, uint256 value);
 
-  // This generates a public event on the blockchain that will notify clients
+  // 授权事件，链上客户端可监听此事件
   event Approval(
     address indexed _owner,
     address indexed _spender,
     uint256 _value
   );
 
-  // This notifies clients about the amount burnt
+  // 销毁事件，记录代币被销毁的地址和数量
   event Burn(address indexed from, uint256 value);
 
   /**
-   * Constructor function
+   * 构造函数
    *
-   * Initializes contract with initial supply tokens to the creator of the contract
+   * 使用初始代币供应量、代币名称和代币符号初始化合约
+   * 部署者会获得全部初始代币
    */
   constructor(
     uint256 initialSupply,
     string memory tokenName,
     string memory tokenSymbol
   ) {
-    totalSupply = initialSupply * 10**uint256(decimals); // Update total supply with the decimal amount
-    balanceOf[msg.sender] = totalSupply; // Give the creator all initial tokens
-    name = tokenName; // Set the name for display purposes
-    symbol = tokenSymbol; // Set the symbol for display purposes
+    // 计算总供应量，乘以 10^decimals 将代币单位转为最小单位
+    totalSupply = initialSupply * 10**uint256(decimals);
+    // 将部署者账户赋予全部初始代币
+    balanceOf[msg.sender] = totalSupply;
+    // 设置代币名称
+    name = tokenName;
+    // 设置代币符号
+    symbol = tokenSymbol;
   }
 
   /**
-   * Internal transfer, only can be called by this contract
+   * 内部转账函数，仅在合约内部调用
+   *
+   * @param _from 转出地址
+   * @param _to 转入地址
+   * @param _value 转账数量
    */
   function _transfer(
     address _from,
     address _to,
     uint256 _value
   ) internal {
-    // Prevent transfer to 0x0 address. Use burn() instead
+    // 不能转账到 0 地址，若要销毁应使用 burn() 接口
     require(_to != address(0x0));
-    // Check if the sender has enough
+    // 检查发送者余额是否足够
     require(balanceOf[_from] >= _value);
-    // Check for overflows
+    // 防止溢出
     require(balanceOf[_to] + _value >= balanceOf[_to]);
-    // Save this for an assertion in the future
+    // 保存转账前两者余额之和，用于后续断言检查一致性
     uint256 previousBalances = balanceOf[_from] + balanceOf[_to];
-    // Subtract from the sender
+    // 从发送者余额扣除
     balanceOf[_from] -= _value;
-    // Add the same to the recipient
+    // 给接收者增加余额
     balanceOf[_to] += _value;
+    // 触发转账事件
     emit Transfer(_from, _to, _value);
-    // Asserts are used to use static analysis to find bugs in your code. They should never fail
+    // 断言转账前后余额总和不变，辅助静态分析发现潜在错误
     assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
   }
 
   /**
-   * Transfer tokens
+   * 转账函数
    *
-   * Send `_value` tokens to `_to` from your account
+   * 将当前调用者的 `_value` 代币发送给 `_to`
    *
-   * @param _to The address of the recipient
-   * @param _value the amount to send
+   * @param _to 接收地址
+   * @param _value 转账数量
    */
   function transfer(address _to, uint256 _value) public returns (bool success) {
     _transfer(msg.sender, _to, _value);
@@ -90,32 +104,35 @@ contract TokenERC20 {
   }
 
   /**
-   * Transfer tokens from other address
+   * 从其他地址转账
    *
-   * Send `_value` tokens to `_to` on behalf of `_from`
+   * 将 `_from` 地址的 `_value` 代币发送给 `_to`
+   * 前提是当前调用者已经获得 `_from` 的授权
    *
-   * @param _from The address of the sender
-   * @param _to The address of the recipient
-   * @param _value the amount to send
+   * @param _from 转出地址
+   * @param _to 接收地址
+   * @param _value 转账数量
    */
   function transferFrom(
     address _from,
     address _to,
     uint256 _value
   ) public returns (bool success) {
-    require(_value <= allowance[_from][msg.sender]); // Check allowance
+    // 检查当前调用者是否被授权足够额度
+    require(_value <= allowance[_from][msg.sender]);
+    // 扣减授权额度
     allowance[_from][msg.sender] -= _value;
     _transfer(_from, _to, _value);
     return true;
   }
 
   /**
-   * Set allowance for other address
+   * 授权其他地址
    *
-   * Allows `_spender` to spend no more than `_value` tokens on your behalf
+   * 允许 `_spender` 花费调用者最多 `_value` 个代币
    *
-   * @param _spender The address authorized to spend
-   * @param _value the max amount they can spend
+   * @param _spender 被授权地址
+   * @param _value 授权额度
    */
   function approve(address _spender, uint256 _value)
     public
@@ -127,13 +144,14 @@ contract TokenERC20 {
   }
 
   /**
-   * Set allowance for other address and notify
+   * 授权并通知
    *
-   * Allows `_spender` to spend no more than `_value` tokens on your behalf, and then ping the contract about it
+   * 允许 `_spender` 花费调用者最多 `_value` 个代币，
+   * 然后调用 `_spender` 合约的 receiveApproval 方法通知它授权完成
    *
-   * @param _spender The address authorized to spend
-   * @param _value the max amount they can spend
-   * @param _extraData some extra information to send to the approved contract
+   * @param _spender 被授权合约地址
+   * @param _value 授权额度
+   * @param _extraData 附加数据，会传给目标合约
    */
   function approveAndCall(
     address _spender,
@@ -148,37 +166,45 @@ contract TokenERC20 {
   }
 
   /**
-   * Destroy tokens
+   * 销毁代币
    *
-   * Remove `_value` tokens from the system irreversibly
+   * 从调用者账户中永久移除 `_value` 个代币，减少总供应量
    *
-   * @param _value the amount of money to burn
+   * @param _value 销毁数量
    */
   function burn(uint256 _value) public returns (bool success) {
-    require(balanceOf[msg.sender] >= _value); // Check if the sender has enough
-    balanceOf[msg.sender] -= _value; // Subtract from the sender
-    totalSupply -= _value; // Updates totalSupply
+    // 检查调用者余额是否足够
+    require(balanceOf[msg.sender] >= _value);
+    // 从余额中扣除销毁数量
+    balanceOf[msg.sender] -= _value;
+    // 总供应量同步减少
+    totalSupply -= _value;
     emit Burn(msg.sender, _value);
     return true;
   }
 
   /**
-   * Destroy tokens from other account
+   * 从其他账户销毁代币
    *
-   * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+   * 在 `_from` 已授权当前调用者的前提下，销毁该账户中的 `_value` 代币
    *
-   * @param _from the address of the sender
-   * @param _value the amount of money to burn
+   * @param _from 代币来源地址
+   * @param _value 销毁数量
    */
   function burnFrom(address _from, uint256 _value)
     public
     returns (bool success)
   {
-    require(balanceOf[_from] >= _value); // Check if the targeted balance is enough
-    require(_value <= allowance[_from][msg.sender]); // Check allowance
-    balanceOf[_from] -= _value; // Subtract from the targeted balance
-    allowance[_from][msg.sender] -= _value; // Subtract from the sender's allowance
-    totalSupply -= _value; // Update totalSupply
+    // 检查目标地址余额是否足够
+    require(balanceOf[_from] >= _value);
+    // 检查当前调用者是否有足够授权额度
+    require(_value <= allowance[_from][msg.sender]);
+    // 从目标地址扣除代币
+    balanceOf[_from] -= _value;
+    // 扣减授权额度
+    allowance[_from][msg.sender] -= _value;
+    // 减少总供应量
+    totalSupply -= _value;
     emit Burn(_from, _value);
     return true;
   }
